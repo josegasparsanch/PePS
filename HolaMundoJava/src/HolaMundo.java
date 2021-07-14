dd9d7009c69a56119473b811e6e808f0b4c2738c
José Gaspar Sánchez Garcia <josegasparsanch@gmail.com>
2021-07-14 19:28:24 +0200

    Confirmación de subida del Proyecto Hola Mundo en Java.

---
+++ HolaMundoJava/src/HolaMundo.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+
+/**
+ * @author José Gaspar
+ *
+ */
+public class HolaMundo {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hola Mundo Java-Jenkins!");
+		System.exit(0);
+	}
+
+}
