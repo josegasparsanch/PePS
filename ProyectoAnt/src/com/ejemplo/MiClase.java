a628543e34b088cbe6e3d899dedca132e120d2cc
José Gaspar Sánchez Garcia <josegasparsanch@gmail.com>
2021-07-15 16:01:40 +0200

    Proyecto Ant de prueba con Eclipse.

---
+++ ProyectoAnt/src/com/ejemplo/MiClase.java
@@ -0,0 +1,10 @@
+package com.ejemplo;
+
+public class MiClase {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("¡Probando Ant y Eclipse!");
+	}
+
+}
